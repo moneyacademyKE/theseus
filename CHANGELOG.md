2445b87139c281755eba50370d8d5e208d2f37a3
Moe <moe@M.local>
2026-09-09 19:24:48 +0300

    release v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## v1.0.0 - 2026-09-09
 
 ### Added
 
--- src/bb_agent/version.clj
+++ src/bb_agent/version.clj
@@ -2,4 +2,4 @@
   "Single source of truth for the running Theseus release. Printed at
   poller boot (bk-173e) so 'what version is running' is answered by the
   log, not by ps and prayer. Bump together with the git tag.")
-(def v "v0.9.0")
+(def v "v1.0.0")
